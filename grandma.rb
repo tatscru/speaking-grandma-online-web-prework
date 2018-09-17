ab010be9548dfa3c58234707e775f980831f57fe
tatiana.mojica <tatiana.mojica@bustle.com>
2018-09-17 15:11:18 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,7 @@
 def speak_to_grandma(string)
   if string == string.upcase
     "NO, NOT SINCE 1938!"
-  elsif string && "I LOVE YOU GRANDMA!"
+  elsif string == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
   else 
     "HUH?! SPEAK UP, SONNY!"
