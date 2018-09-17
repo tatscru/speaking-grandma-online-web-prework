4a5285fdc762883a7cb54c76cc42eb8e57072e1b
tatiana.mojica <tatiana.mojica@bustle.com>
2018-09-17 14:53:02 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -17,6 +17,8 @@ def speak_to_grandma(string)
     puts "NO, NOT SINCE 1938!"
   elsif string == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
+  else 
+    puts "HUH?! SPEAK UP, SONNY!"
   end
 end
 
