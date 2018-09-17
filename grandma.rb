b25b033dca5d3c16a21dd12d9dccb539eb110051
tatiana.mojica <tatiana.mojica@bustle.com>
2018-09-17 15:17:45 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -22,14 +22,5 @@ def speak_to_grandma(string)
   end
 end
 
-#     puts "I LOVE YOU TOO PUMPKIN!"
-#   elsif speak_to_grandma
-#     puts 
-#   elseif speak_to_grandma
-#     puts "HUH?! SPEAK UP, SONNY!"
-#   elseif speak_to_grandma
-#     puts "HUH?! SPEAK UP, SONNY"
-#   else speak_to_grandma
-#     puts "NO, NOT SINCE 1938!"
-# end
+
   
