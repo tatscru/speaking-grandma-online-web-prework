7df571b92710bc4b496679d3d7c7ceb64a1161fd
tatiana.mojica <tatiana.mojica@bustle.com>
2018-09-17 14:59:28 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -13,7 +13,7 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(string)
-  if string.downcase
+  if string.upcase
     "NO, NOT SINCE 1938!"
   elsif string && "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
