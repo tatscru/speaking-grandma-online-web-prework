236cefa3842ba4432865c680baceb0a3058a5b66
tatiana.mojica <tatiana.mojica@bustle.com>
2018-09-17 14:49:38 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -14,7 +14,7 @@
 
 def speak_to_grandma(string)
   if string.upcase
-    puts "HUH?! SPEAK UP, SONNY"
+    puts "NO, NOT SINCE 1938!"
   end
 end
 
@@ -24,7 +24,7 @@ end
   elseif speak_to_grandma
     puts "HUH?! SPEAK UP, SONNY!"
   elseif speak_to_grandma
-    puts "NO, NOT SINCE 1938!"
+    puts "HUH?! SPEAK UP, SONNY"
   else speak_to_grandma
     puts "NO, NOT SINCE 1938!"
 end
